cb6fe185d8cf43f9609b560f7cc697a6b19dc638
Eladielle <zloxomyak@gmail.com>
2024-01-29 04:52:24 +0100

    addmuimaterial

--- pet-app-front/src/features/auth/Register.tsx
+++ pet-app-front/src/features/auth/Register.tsx
@@ -1,5 +1,7 @@
 import { useCallback, useState } from "react"
 import type { ChangeEvent, FormEvent } from "react"
+import { Box, Button, IconButton, InputAdornment, Link, TextField, } from "@mui/material"
+import { Visibility, VisibilityOff } from '@mui/icons-material'
 import { useSelector } from "react-redux"
 import { useLocation, useNavigate } from "react-router-dom"
 import { useAppDispatch } from "../../app/hooks"
@@ -69,56 +71,88 @@ const handlePasswordRepeatChange = useCallback(
 );
  
   return (
+    <Box sx={{ fontFamily: 'Literata', my: 2 }}>
     <form className="auth-form" onSubmit={handleSubmit}>
-    <h2>Register</h2>
-    {error && (
-      <div className="invalid-feedback rm" style={{ display: 'block' }}>
-        {error}
-      </div>
-    )}
-    <div className="rm">
-      <label htmlFor="email-input" className="form-label">
-        Email
-      </label>
-      <input
-        type="text"
-        className={`form-control ${error ? 'is-invalid' : ''}`}
-        id="name-input"
-        name="email"
-        value={email}
-        onChange={handleEmailChange}
-      />
-    </div>
-    <div className="rm">
-      <label htmlFor="password-input" className="form-label">
-        Password
-      </label>
-      <input
-        type="password"
-        className={`form-control ${error ? 'is-invalid' : ''}`}
-        id="password-input"
-        name="password"
-        value={password}
-        onChange={handlePasswordChange}
-        
-      />
-    </div>
-    <div className="mb-3">
-      <label htmlFor="password-repeat-input" className="form-label">
-        Repeat password
-      </label>
-      <input
-        type="password"
-        className={`form-control ${error ? 'is-invalid' : ''}`}
-        id="password-repeat-input"
-        name="passwordRepeat"
-        value={passwordRepeat}
-        onChange={handlePasswordRepeatChange}
-      />
-    </div>
-    <button type="submit" className="btn btn-primary">
-      Submit
-    </button>
-  </form>
-  )
+      <Box sx={{ fontSize: 40, fontWeight: 500, textAlign: 'center' }}>
+        Registration page
+      </Box>
+      <Box sx={{ margin: '0 auto'  }}>
+        <TextField
+          autoComplete="false"
+          required
+          fullWidth
+          color="secondary"
+          margin="normal"
+          label="Email"
+          variant="outlined"
+          value={email}
+          onChange={handleEmailChange}
+        />
+      </Box>
+      <Box sx={{ margin: '10 auto' }}>
+        <TextField
+          required
+          fullWidth
+          color="secondary"
+          margin="normal"
+          id="password-input"
+          label="Password"
+          variant="outlined"
+          type={showPassword ? 'text' : 'password'}
+          value={password}
+          onChange={handlePasswordChange}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleTogglePasswordVisibility}
+                  edge="end"
+                >
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
+        />
+      </Box>
+      <Box sx={{ margin: '0 auto' }}>
+        <TextField
+          required
+          fullWidth
+          color="secondary" 
+          margin="normal"
+          id="password-repeat-input"
+          label="Repeat password"
+          variant="outlined"
+          type={showPassword ? 'text' : 'password'}
+          value={passwordRepeat}
+          onChange={handlePasswordRepeatChange}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleTogglePasswordVisibility}
+                  edge="end"
+                >
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
+        />
+      </Box>
+      <Box sx={{ textAlign: 'center', my: 3 }}>
+        <Button variant="contained" type="submit" color="secondary">
+          Sign up
+        </Button>
+        {error && <Box sx={{ display: 'block' }}>{error}</Box>}
+      </Box>
+      <Box sx={{ textAlign: 'center' }}>
+        <Link href="/login" color="secondary">Already registered? Login</Link>
+      </Box>
+    </form>
+  </Box>
+);
 }
