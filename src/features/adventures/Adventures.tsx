1d72568f29c9bcb2d83c9a29e38c9e58acc1152b
CTAC-AIT-TR <justas@unitybox.de>
2024-02-05 22:55:10 +0100

    update adventures code

--- src/features/adventures/Adventures.tsx
+++ src/features/adventures/Adventures.tsx
@@ -9,10 +9,57 @@ export default function Adventures(): JSX.Element {
 					<Navigation />
 					<div className="relative w-full h-full">
 						<div>
-							<img src="/img/img_colony.jpg" alt="Colony: Code Red" />
+							<img src="/img/img_kernel.jpg" alt="Kernel: Confrontation" />
 						</div>
 						<div className="absolute flex-col left-32 top-[200px] w-7/12 text-secondary">
 							<div className="text-[42px] font-bold">GAMES</div>
+							<div className="text-[28px] font-bold">Kernel: Confrontation</div>
+							<div className="v_lined left-0 px-0 py-5">
+								&nbsp; GENRE
+								<br />
+								&nbsp; PvP
+							</div>
+							<div className="v_lined left-0 px-20">
+								&nbsp; SESSION
+								<br />
+								&nbsp; 60 min
+							</div>
+							<div className="v_lined left-0 px-0">
+								&nbsp; PLAYERS
+								<br />
+								&nbsp; 2+
+							</div>
+							<div className="v_lined left-0 px-20">
+								&nbsp; AGE
+								<br />
+								&nbsp; 8+
+							</div>
+							<div className=" text-base">
+								BEYOND REALMS is happy to present its new hyper-realistic
+								shooter - KERNEL. Two teams will fight on the territory of an
+								abandoned bunker where abnormal things take place: the
+								mysterious black sun changes the laws of physics.
+							</div>
+							<div className="relative">
+								<div className="absolute flex top-[300px] left-[50%] gap-x-6 -translate-y-64 -translate-x-1/2 transform-gpu text-nowrap text-secondary">
+									<button className="btn-lg rounded py-2 px-6 btn-rounded bg-primary sm:btn-sm md:btn-md lg:btn-lg hover:bg-rose-500 text-secondary text-sm">
+										<a href="/games">MORE DETAILS</a>
+									</button>
+									<button className="btn-lg  rounded py-2 px-6 btn-rounded bg-primary sm:btn-sm md:btn-md lg:btn-lg hover:bg-rose-500 text-secondary text-sm">
+										<a href="/experience">BOOK YOUR EXPERIENCE</a>
+									</button>
+									<button className="btn-lg  rounded py-2 px-6 btn-rounded bg-primary sm:btn-sm md:btn-md lg:btn-lg hover:bg-rose-500 text-secondary text-sm">
+										<a href="/video/29en.webm">WATCH TRAILER</a>
+									</button>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div className="relative w-full h-full">
+						<div>
+							<img src="/img/img_colony.jpg" alt="Colony: Code Red" />
+						</div>
+						<div className="absolute flex-col left-32 top-[200px] w-7/12 text-secondary">
 							<div className="text-[28px] font-bold">Colony: Code Red</div>
 							<div className="v_lined left-0 px-0 py-5">
 								&nbsp; GENRE
