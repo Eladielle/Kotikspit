002ce8cd4dcc32733f8789f829ecb3f90717a5e0
CTAC-AIT-TR <justas@unitybox.de>
2024-02-08 11:19:19 +0100

    update experienc/buy page

--- src/features/experience/Buy.tsx
+++ src/features/experience/Buy.tsx
@@ -1,17 +1,50 @@
-import type { RootState } from '../../app/store'
-import { useEffect } from 'react'
-import { useAppSelector, useAppDispatch } from '../../app/hooks'
+import { RootState } from '../../app/store'
+import { ChangeEvent, FormEvent, useEffect, useState } from 'react'
+import { useAppDispatch, useAppSelector } from '../../app/hooks'
 import Navigation from '../../components/navigation/navMainPage'
 import { loadPageBuy } from './PageBuySlice'
 import './styles.buy.css'
+import PageBuy from './type/PageBuy'
 
 export default function Buy(): JSX.Element {
-	const games = useAppSelector((state: RootState) => state.news)
+	const pageBuy = useAppSelector((state: RootState) => state.experience.pageBuy)
 
 	const dispatch = useAppDispatch()
 	useEffect(() => {
 		dispatch(loadPageBuy())
-	}, [])
+	}, [dispatch])
+
+	const [selectedGame, setSelectedGame] = useState<
+		PageBuy['content'][0] | null
+	>(null)
+	const [selectedDate, setSelectedDate] = useState<Date | null>(null)
+	const [selectedTime, setSelectedTime] = useState('')
+	const [numPlayers, setNumPlayers] = useState<number>(1)
+
+	const handleGameChange = (event: ChangeEvent<HTMLSelectElement>): void => {
+		const gameId = parseInt(event.target.value)
+		const gameSelected = pageBuy.content.find(game => game.id === gameId)
+		setSelectedGame(gameSelected || null)
+	}
+
+	const handleDateChange = (date: Date | null): void => {
+		setSelectedDate(date)
+	}
+
+	const handleTimeChange = (event: ChangeEvent<HTMLInputElement>): void => {
+		setSelectedTime(event.target.value)
+	}
+
+	const handleNumPlayersChange = (
+		event: ChangeEvent<HTMLInputElement>
+	): void => {
+		setNumPlayers(parseInt(event.target.value))
+	}
+
+	const handleSubmit = (event: FormEvent<HTMLFormElement>): void => {
+		event.preventDefault()
+		// Add your form submission logic here
+	}
 
 	return (
 		<>
@@ -24,30 +57,46 @@ export default function Buy(): JSX.Element {
 					<div className="text-secondary text-center text-[52px] font-extrabold">
 						8 adventures
 					</div>
-					<form className="form-container">
+					<form className="form-container" onSubmit={handleSubmit}>
 						<label>
 							Select a game:
-							<select>
+							<select
+								value={selectedGame ? selectedGame.id : ''}
+								onChange={handleGameChange}
+							>
 								<option value="">Select</option>
-								{games.pageNews.content.map(game => (
-									<option key={game.title} value={game.title}>
-										{game.title}
+								{pageBuy.content.map(game => (
+									<option key={game.id} value={game.id}>
+										{game.title} - Price: {game.price} - Players:{' '}
+										{game.numberOfPlayers}
 									</option>
 								))}
 							</select>
 						</label>
 						<label>
-							Select a date:
-							<input type="date" />
+							Select a date: {selectedDate && selectedDate.toDateString()}
+							<input
+								type="date"
+								onChange={e => handleDateChange(new Date(e.target.value))}
+							/>
 						</label>
 						<label>
-							Select playing time (minutes):
-							<input type="number" />
+							Select playing time (minutes): {selectedTime}
+							<input
+								type="number"
+								value={selectedTime}
+								onChange={handleTimeChange}
+							/>
 						</label>
 						<label>
-							Number of players:
-							<input type="number" />
+							Number of players: {numPlayers}
+							<input
+								type="number"
+								value={numPlayers}
+								onChange={handleNumPlayersChange}
+							/>
 						</label>
+
 						<button className="btn-primary hover:bg-rose-500" type="submit">
 							BOOK AN EXPERIENCE
 						</button>
